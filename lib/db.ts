d6cd93b8a89c0109378f5546be5f12bfc25484c0
Sachin Prajapati <skprajapati3214@gmail.com>
2025-02-01 15:55:44 +0530

    Add MongoDB connection setup and update dependencies

---
+++ lib/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error("Please define mongodb uri in env file");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+            maxPoolSize: 5,
+            bufferMaxEntries: 0,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI as string, opts).then((mongoose) => {
+            return mongoose.connection;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+}
